fc52e77c1ad523c8b36368769c83017271b91829
Roberval Fernandes <robervalfernandesdigital@outlook.com>
2024-11-30 11:11:04 -0300

    Adicionando métodos completos

--- Projeto-G/Gerenciador.py
+++ Projeto-G/Gerenciador.py
@@ -12,6 +12,23 @@ def ver_tarefas(tarefas):
             status = "✓" if tarefa["completada"] else " "
             nome_tarefa = tarefa["tarefa"]
             print(f"{indice}. [{status}] {nome_tarefa}")
+            
+def atualizar_nome_tarefas(tarefas, indice_tarefa, novo_nome_tarefa):
+    indice_tarefa_ajustado = int(indice_tarefa) - 1
+    if indice_tarefa_ajustado >=0 and indice_tarefa_ajustado < len(tarefas):
+        tarefas[indice_tarefa_ajustado]["tarefa"] = novo_nome_tarefa
+        print(f"Tarefa {indice_tarefa} atualizada para {novo_nome_tarefa}")
+    else:
+        print("Indice de tarefa invalido")
+    return
+
+def completar_tarefa(tarefas, indice_tarefa):
+    indice_tarefa_ajustado = int(indice_tarefa) - 1
+    tarefas[indice_tarefa_ajustado]["completada"] = True
+    print(f"Tarefa {indice_tarefa} marcada como completada")
+    return
+
+tarefas = []
 
 while True:
     print("\nMenu do Gerenciador de Lista de tarefas: ")
@@ -21,3 +38,30 @@ while True:
     print("4. Completar Tarefa")
     print("5. Deletar tarefas completadas")
     print("6. Sair")
+    
+    escolha = input("Digite a sua escolha: ")
+
+    if escolha == "1":
+        nome_tarefa = input("Digite o nome da tarefa que deseja adicionar:\n ")
+        adicionar_tarefa(tarefas, nome_tarefa)
+
+    elif escolha == "2":
+        ver_tarefas(tarefas)
+
+    elif escolha =="3":
+        ver_tarefas(tarefas)
+        indice_tarefa = input("Digite o número da tarefa que deseja atualizar: ")
+        novo_nome = input("Digite o novo nome da tarefa: ")
+        atualizar_nome_tarefas(tarefas, indice_tarefa,novo_nome)
+        
+    elif escolha == "4":
+        ver_tarefas(tarefas)
+        indice_tarefa = input("Digite o numero da tarefa que deseja completar: ")
+        completar_tarefa(tarefas, indice_tarefa)    
+        
+    elif escolha == "6":
+        print("Programa finalizado.")
+        break
+
+    else:
+        print("Opção inválida! Tente novamente.")
