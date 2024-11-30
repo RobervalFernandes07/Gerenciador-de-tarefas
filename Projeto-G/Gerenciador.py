e4e73bda35513b650c7bb36826151601155ec419
Roberval Fernandes <robervalfernandesdigital@outlook.com>
2024-11-30 11:06:25 -0300

    Adicionando tarefas

---
+++ Projeto-G/Gerenciador.py
@@ -0,0 +1,8 @@
+while True:
+    print("\nMenu do Gerenciador de Lista de tarefas: ")
+    print("1. Adicionar tarefa")
+    print("2. Ver tarefas")
+    print("3. Atualizar Tarefas ")
+    print("4. Completar Tarefa")
+    print("5. Deletar tarefas completadas")
+    print("6. Sair")
