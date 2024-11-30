4f392440b51db8755b4c331ddadef5ae52b4186e
Roberval Fernandes <robervalfernandesdigital@outlook.com>
2024-11-30 11:07:29 -0300

    Adicionado funções

--- Projeto-G/Gerenciador.py
+++ Projeto-G/Gerenciador.py
@@ -1,3 +1,18 @@
+def adicionar_tarefa(tarefas, nome_tarefa):
+    tarefa = {"tarefa": nome_tarefa, "completada": False}
+    tarefas.append(tarefa)
+    print(f"Tarefa '{nome_tarefa}' foi adicionada com sucesso!")
+
+def ver_tarefas(tarefas):
+    print("\nLista de tarefas:")
+    if not tarefas:  # Verifica se a lista está vazia
+        print("Nenhuma tarefa cadastrada.")
+    else:
+        for indice, tarefa in enumerate(tarefas, start=1):
+            status = "✓" if tarefa["completada"] else " "
+            nome_tarefa = tarefa["tarefa"]
+            print(f"{indice}. [{status}] {nome_tarefa}")
+
 while True:
     print("\nMenu do Gerenciador de Lista de tarefas: ")
     print("1. Adicionar tarefa")
